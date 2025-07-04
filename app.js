7dc5e338a57c856772695c4af39791bfd2dfe547
Petar Rakocevic <petar.r.rakocevic@gmail.com>
2025-07-04 15:28:36 +0200

    Health Check

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
-
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
